d4bb28c6992f21c54b9129908daf0550de6e73d4
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-02 12:32:51 -0300

    feat: create redux store

--- src/main.jsx
+++ src/main.jsx
@@ -3,11 +3,19 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 
 import { BrowserRouter as Router} from "react-router-dom";
+import { Provider } from "react-redux";
+
+import { createStore } from "redux";
+import { rootReducer } from "./reducers";
+
+const store = createStore(rootReducer);
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <Router>
-      <App />
+      <Provider store={store}>
+        <App />
+      </Provider>
     </Router>
   </React.StrictMode>
 )
