4e72d14414f6aa08d5776f4567cc370ea7b9d3df
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-02 14:54:05 -0300

    feat: create function to handle to the form

--- src/components/FormPlayer/index.jsx
+++ src/components/FormPlayer/index.jsx
@@ -1,3 +1,5 @@
+import { useState, useEffect } from "react";
+
 import { useDispatch } from "react-redux/es/exports";
 
 import { Modal, ModalTitle, ModalButtonClose } from "../../styles/modal";
@@ -8,38 +10,83 @@ const FormPlayer = () => {
     const dispatch = useDispatch();
     const closeModal = () => dispatch({type: "modal/players"});
 
+    const [name, setName] = useState("");
+    const [level, setLevel] = useState(1);
+    const [handleError, setHandleError] = useState({name: false, level: false})
+
+    const onHandleInputName = (event) => setName(event.target.value);
+    const onHandleInputLevel = (event) => setLevel(+event.target.value);
+
+    const validateInputName = () => {
+        const error = name.length < 2 ? "Name must have 2 or more characters." : false;
+        setHandleError({...handleError, name: error})
+    }
+
+    const validateInputLevel = () => {
+        const error = level < 1 || level > 5 ? "Level must be between 1 and 5." : false;
+        setHandleError({...handleError, level: error})
+    }
+
+    const onHandleSubmit = () => {
+        event.preventDefault();
+
+        if(!handleError.name && !handleError.level){
+            alert("Hey, everthing okay with the register :)")
+            console.log(name)
+            console.log(level)
+        }else{
+            alert("Hey, something wrong with the register :(")
+        }
+    }
+
+    useEffect(() => {
+        validateInputName();
+    },[name])
+
+    useEffect(() => {
+        validateInputLevel();
+    },[level])
+    
     return(
         <Modal>
             <header>
                 <ModalTitle>ADD <b>PLAYER</b></ModalTitle>
-
                 <ModalButtonClose onClick={closeModal}>
                     <span class="material-symbols-outlined">close</span>
                 </ModalButtonClose>
             </header>
 
-            <Form>
+            <Form onSubmit={onHandleSubmit}>
                 <GroupLabelInput>
                     <label>Name:</label>
-                    <input type="text" name="name" />
-                    <span>an error can appears here</span>
-                </GroupLabelInput>
-
-                <GroupLabelInput>
-                    <label>Age:</label>
-                    <input type="number" name="age" />
-                    <span>an error can appears here</span>
+                    <input 
+                        type="text" 
+                        name="name" 
+                        value={name}
+                        minLength={2}
+                        maxLength={20}
+                        required
+                        onChange={(event) => onHandleInputName(event)}
+                    />
+                    {handleError.name && <span>{handleError.name}</span>}
                 </GroupLabelInput>
 
                 <GroupLabelInput>
                     <label>Level:</label>
-                    <input type="number" name="level" />
-                    <span>an error can appears here</span>
+                    <input 
+                        type="number" 
+                        name="level" 
+                        value={level}
+                        minLength={1} 
+                        maxLength={5} 
+                        required
+                        onChange={(event) => onHandleInputLevel(event)}
+                    />
+                    {handleError.level && <span>{handleError.level}</span>}
                 </GroupLabelInput>
 
-                <FormButton>Register</FormButton>
+                <FormButton type="submit">Register</FormButton>
             </Form>
-
         </Modal>
     )
 }
