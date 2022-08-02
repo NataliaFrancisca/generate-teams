db2f51a0fd71b964287665e0e6f67f5cb68fb05a
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-02 14:21:07 -0300

    style: create modal styled

---
+++ src/styles/modal.jsx
@@ -0,0 +1,53 @@
+import styled from "styled-components";
+
+export const Modal = styled.div`
+    position: absolute;
+    height: auto;
+    width: 60%;
+
+    border-radius: 3px;
+    padding: 20px;
+    background-color: var(--colorBackground);
+
+    transform: translate(-50%, -50%);
+    top: 50%;
+    left: 50%;
+
+    z-index: 11;
+
+    header{
+        display: flex;
+        justify-content: space-between;
+    }
+`
+
+export const ModalTitle = styled.h1`
+    font-size: 1.4em;
+    font-family: var(--title);
+    font-weight: 400;
+
+    text-align: center;
+    color: rgb(27, 38, 44);
+
+    margin-bottom: 8px;
+    width: max-content;
+
+    &:after{
+        content: "";
+        height: 3px;
+        width: 80px;
+        background-color: #C4DFAA;
+        top: 14px;
+        display: block;
+    }
+`
+export const ModalButtonClose = styled.button`
+    background-color: transparent;
+    font-size: 1.4em;
+
+    cursor: pointer;
+
+    &:hover{
+        color: var(--colorPlayers)
+    }
+`
