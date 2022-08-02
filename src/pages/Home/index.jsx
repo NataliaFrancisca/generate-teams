c0a8a628b82c4a549299058bf7a4166b605d2693
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-02 10:44:34 -0300

    feat: create function to navigate to the others pages

--- src/pages/Home/index.jsx
+++ src/pages/Home/index.jsx
@@ -1,11 +1,17 @@
 import { HomeStyled, HomeTitle, Button } from "./style";
+import { useNavigate } from "react-router-dom";
 
 const Home = () => {
+
+    const navigate = useNavigate();
+    const onNavigatePlayersPage = () => navigate("/players");
+    const onNavigateTeamsPage = () => navigate("/teams");
+    
     return(
         <HomeStyled>
             <HomeTitle>CREATE YOUR <b>TEAMS</b> EASILY</HomeTitle>
-            <Button backgroundColor="#AFB4FF">REGISTER YOUR PLAYERS</Button>
-            <Button backgroundColor="#B1E1FF">CREATE YOUR TEAMS</Button>
+            <Button backgroundColor="#AFB4FF" onClick={onNavigatePlayersPage}>REGISTER YOUR PLAYERS</Button>
+            <Button backgroundColor="#B1E1FF" onClick={onNavigateTeamsPage}>CREATE YOUR TEAMS</Button>
         </HomeStyled>
     )
 }
