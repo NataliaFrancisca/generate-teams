32e0d6a5ef05738d4555e797aa0ce02612a33f29
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-02 11:57:09 -0300

    style: create FloatButton

--- src/styles/elements.jsx
+++ src/styles/elements.jsx
@@ -3,3 +3,24 @@ import styled from "styled-components";
 export const ContainerPage = styled.section`
     display: flex;
 `
+
+export const FloatButton = styled.button`
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
+
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+
+    box-shadow: 0px 0px 2px rgba(20,20,20,.5);
+    background-color: #C4DFAA;
+
+    transition: filter .6s ease-in-out;
+
+    cursor: pointer;
+
+    &:hover{
+        filter: brightness(80%);
+    }
+`
