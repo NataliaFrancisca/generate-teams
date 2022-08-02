70c246d6c6010be9cea9649fe9d90f60e3017f1c
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-02 16:04:37 -0300

    feat: add actions button and function that delete player

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,12 +1,23 @@
+import { ButtonAction } from "../../styles/elements";
+
+import { deletePlayer } from "../../functions/registerPlayer";
 const Player = ({playerData}) => {
 
     const {name, level} = playerData;
+
+    const onDeletePlayer = () => {
+        deletePlayer(playerData.id)
+        window.dispatchEvent(new Event("storage"));
+    }
 
     return(
         <tr>
             <td>{name}</td>
             <td>{level}</td>
-            <td>actions</td>
+            <td>
+                <ButtonAction color="#f5f8ff"><span class="material-symbols-outlined">edit</span></ButtonAction>
+                <ButtonAction onClick={onDeletePlayer} color="#EB4747"><span class="material-symbols-outlined">delete</span></ButtonAction>
+            </td>
         </tr>
     )
 }
