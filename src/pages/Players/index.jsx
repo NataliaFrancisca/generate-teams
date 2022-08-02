479cec0daf76ebf77e1268f45bf91f36557fcb60
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-02 11:23:40 -0300

    feat: create players page and add table

--- src/pages/Players/index.jsx
+++ src/pages/Players/index.jsx
@@ -1,8 +1,50 @@
+import { PlayerStyled, TitlePlayers, ContainerTable } from "./style";
+
 const Players = () => {
     return(
-        <section>
-            <h1>Players</h1>
-        </section>
+        <PlayerStyled>
+            <TitlePlayers>YOUR <b>PLAYERS</b></TitlePlayers>
+           
+            <ContainerTable>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Nome</th>
+                            <th>Level</th>
+                            <th>Actions</th>
+                        </tr>
+                    </thead>
+
+                    <tbody>
+                        <tr>
+                            <td>Natalia</td>
+                            <td>4</td>
+                            <td>Natalia</td>
+                        </tr>
+
+                        <tr>
+                            <td>Natalia</td>
+                            <td>4</td>
+                            <td>Natalia</td>
+                        </tr>
+
+                        <tr>
+                            <td>Natalia</td>
+                            <td>4</td>
+                            <td>Natalia</td>
+                        </tr>
+
+                        <tr>
+                            <td>Natalia</td>
+                            <td>4</td>
+                            <td>Natalia</td>
+                        </tr>
+                    </tbody>
+                </table>
+            </ContainerTable>
+        </PlayerStyled>
+
+       
     )
 }
 
