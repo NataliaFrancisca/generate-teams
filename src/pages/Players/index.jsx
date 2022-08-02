ff830e3f0724b9d58c95c88722001b53ec7157bb
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-02 15:43:20 -0300

    feat: get players from localStorage

--- src/pages/Players/index.jsx
+++ src/pages/Players/index.jsx
@@ -1,19 +1,30 @@
+import { useEffect, useState } from "react";
+import { useSelector, useDispatch } from "react-redux";
+
 import Player from "../../components/Player";
+import FormPlayer from "../../components/FormPlayer";
 
 import { FloatButton, BackgroundMask} from "../../styles/elements";
 import { PlayersStyled, TitlePlayers, ContainerTable } from "./style";
 
-import FormPlayer from "../../components/FormPlayer";
-
-import { useSelector, useDispatch } from "react-redux";
+import { getPlayers } from "../../functions/registerPlayer";
 
 const Players = () => {
 
+    const [statePlayers, setStatePlayers] = useState();
     const dispatch = useDispatch();
 
     const modalState = useSelector(state => state.reducerModalPlayers);
     const openModal = () => dispatch({type: "modal/players"});
 
+    window.addEventListener('storage', () => {
+        setStatePlayers(getPlayers());
+    })
+
+    useEffect(() => {
+        setStatePlayers(getPlayers());
+    },[])
+  
     return(
         <PlayersStyled>
             <BackgroundMask filter={modalState} />
@@ -28,13 +39,11 @@ const Players = () => {
                             <th>Actions</th>
                         </tr>
                     </thead>
-
                     <tbody>
-                      <Player playerData={{name: "Natalia", level: 3}} />
-                      <Player playerData={{name: "Luiz", level: 3}} />
-                      <Player playerData={{name: "Fernanda", level: 3}} />
-                    </tbody>
-                    
+                        {statePlayers.map(player => (
+                            <Player key={player.id} playerData={player} />
+                        ))}
+                    </tbody>  
                 </table>
             </ContainerTable>
 
