84b4c21161ec074856e827691b8a91c82da49b27
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-05 16:16:21 -0300

    feat: get teams and show them

--- src/pages/Teams/index.jsx
+++ src/pages/Teams/index.jsx
@@ -1,13 +1,39 @@
+import { useState, useEffect } from "react";
+
 import { TitlePage } from "../../styles/elements";
 import { TeamsStyled } from "./style";
 
 import FormTeam from "../../components/FormTeam";
 
+import { getPlayersLeftOver, getTeams } from "../../functions/registerTeams";
+import Team from "../../components/Team";
+
 const Teams = () => {
+
+    const [teams, setTeams] = useState();
+    const [playersLeftOver, setPlayersLeftOver] = useState([]);
+  
+    useEffect(() => {
+        updateStates();
+    },[])
+
+    window.addEventListener('storage', () => {
+        updateStates();
+    })
+
+    const updateStates = () => {
+        setTeams(getTeams());
+        setPlayersLeftOver(getPlayersLeftOver())
+    }
+
     return(
         <TeamsStyled>
             <TitlePage bg={'teams'}>YOUR <b>TEAMS</b></TitlePage>
             <FormTeam />
+
+            {teams && teams.map((team, index) => (
+                <Team numberTeam={index} data={team}/>
+            ))}
         </TeamsStyled>
     )
 }
