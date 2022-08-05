530ddc244ba0ca37b135575ec505bb7b9b3420f6
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-05 09:53:30 -0300

    feat: apply function edit player and open modal

--- src/components/Player/index.jsx
+++ src/components/Player/index.jsx
@@ -1,13 +1,21 @@
-import { ButtonAction } from "../../styles/elements";
+import { useDispatch } from "react-redux/es/exports";
 
+import { ButtonAction } from "../../styles/elements";
 import { deletePlayer } from "../../functions/registerPlayer";
-const Player = ({playerData}) => {
+
+const Player = ({playerData, onSendState}) => {
 
     const {name, level} = playerData;
+    const dispatch = useDispatch();
 
     const onDeletePlayer = () => {
         deletePlayer(playerData.id)
         window.dispatchEvent(new Event("storage"));
+    }
+
+    const onEditPlayer = () => {
+        onSendState(playerData);
+        dispatch({type: "modal/edit/player"});
     }
 
     return(
@@ -15,7 +23,7 @@ const Player = ({playerData}) => {
             <td>{name}</td>
             <td>{level}</td>
             <td>
-                <ButtonAction color="#f5f8ff"><span class="material-symbols-outlined">edit</span></ButtonAction>
+                <ButtonAction onClick={onEditPlayer} color="#f5f8ff"><span class="material-symbols-outlined">edit</span></ButtonAction>
                 <ButtonAction onClick={onDeletePlayer} color="#EB4747"><span class="material-symbols-outlined">delete</span></ButtonAction>
             </td>
         </tr>
