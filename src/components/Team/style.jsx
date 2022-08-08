7528b197e9395c6f1cf6365232c1a238e26b0dea
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-08 11:53:42 -0300

    style: use only the team number

--- src/components/Team/style.jsx
+++ src/components/Team/style.jsx
@@ -7,56 +7,46 @@ export const TeamStyled = styled.div`
     width: 100%;
     height: auto;
 
-    padding: 30px 15px;
+    padding: 0 16px 16px 16px;
     margin: 10px 0;
     border-radius: 5px;
 
-    background-color: #cf4fa0;
+    background-color: var(--colorTeams);
     box-shadow: 0px 0px 4px rgba(240,240,240,.4);
 
     position: relative;
     border: solid black 2px;
 
-    & .card-team-number{
+    & h1{
         display: flex;
-        align-items: center;
         justify-content: center;
         align-self: flex-end;
-        margin-bottom: 10px;
-        flex-wrap: wrap;
 
         background-color: #cfa01e;
         border-radius: 0 0 6px 6px;
         width: auto;
         padding: 0 10px;
 
-        position: absolute;
-        top: 0;
-
-        h1{
-            background-color: transparent;
-            color: #F9F2ED;
-            font-family: var(--title);
-            text-align: center;
-            font-size: 2em;
-            word-break: break-all;
-        }
+        color: #F9F2ED;
+        font-family: var(--title);
+        text-align: center;
+        font-size: 2.5em;
+        word-break: break-all;
     }
-
 
     & .card-team-details{
         height: auto;
         display: flex;
         flex-direction: column;
 
-
         h2{
             background-color: #F9F2ED;
-            padding: 10px 5px;
+            padding: 8px 5px;
             margin-top: 10px;
             border-radius: 5px;
             color: #000000;
             border: solid black 2px;
+            font-size: 1.2em;
         }
 
     }
