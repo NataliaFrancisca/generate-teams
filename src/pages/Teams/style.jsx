543e4e58116fc49d1cdbbb4208201aaa565de47b
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-12 11:03:41 -0300

    style: playersleftover styles

--- src/pages/Teams/style.jsx
+++ src/pages/Teams/style.jsx
@@ -20,3 +20,33 @@ export const GridTeams = styled.div`
         grid-template-columns: repeat(auto-fit, minmax(250px, auto));
     }
 `
+
+export const PlayersLeftOver = styled.div`
+    display: grid;
+    grid-template-columns: repeat(auto-fit, minmax(300px, auto));
+    gap: 10px;
+
+    width: 100%;
+    height: auto;
+
+    padding: 10px;
+    margin: 10px 0;
+    border-radius: 5px;
+
+    background-color: #F94C66;
+    box-shadow: 0px 0px 4px rgba(240,240,240,.4);
+
+    border: solid black 2px;
+
+    & h2{ 
+        width: 100%;
+        background-color: #FFFFFF;
+        border-radius: 5px;
+        border: solid black 2px;
+        padding: 10px;
+    }
+
+    @media screen and (max-width: 400px){
+        grid-template-columns: repeat(auto-fit, minmax(250px, auto));
+    }
+`
