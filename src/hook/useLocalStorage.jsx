ffaf6e050b104f191ffbb95dd7607c82d6716e7c
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-12 11:02:20 -0300

    feat: create hook useLocalStorage

---
+++ src/hook/useLocalStorage.jsx
@@ -0,0 +1,23 @@
+import { useState, useCallback } from "react";
+
+export const useLocalStorage = (key, initialValue = "") => {
+    const [state, setState] = useState(() => {
+        try{
+            const storedValue = localStorage.getItem(key);
+            return storedValue ? JSON.parse(storedValue) : initialValue;
+        } catch {
+            return initialValue;
+        }
+    });
+
+    const setValue = useCallback((value) => {
+        try{
+            setState(value);
+            localStorage.setItem(key, JSON.stringify(value));
+        }catch (error){
+            console.log(error);
+        }
+    },[key])
+
+    return [state, setValue];
+}
