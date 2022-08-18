697c8fa4decee05d7b7a764eb7eb433950c70880
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-18 14:18:17 -0300

    style: change components for ids

--- src/styles/modal.jsx
+++ src/styles/modal.jsx
@@ -15,43 +15,44 @@ export const Modal = styled.div`
 
     z-index: 11;
 
-    header{
+    & header{
         display: flex;
         justify-content: space-between;
     }
 
+    & #title-modal{
+        font-size: 1.4em;
+        font-family: var(--title);
+        font-weight: 400;
+
+        text-align: center;
+        color: rgb(27, 38, 44);
+
+        margin-bottom: 8px;
+        width: max-content;
+
+        &:after{
+            content: "";
+            height: 3px;
+            width: 80px;
+            background-color: #C4DFAA;
+            top: 14px;
+            display: block;
+        }
+    }
+
+    & #button-close-modal{
+        background-color: transparent;
+        font-size: 1.4em;
+        cursor: pointer;
+
+        &:hover{
+            color: var(--colorPlayers)
+        }
+    }
+
+
     @media screen and (max-width: 800px) {
         width: 90%;
-    }
-`
-
-export const ModalTitle = styled.h1`
-    font-size: 1.4em;
-    font-family: var(--title);
-    font-weight: 400;
-
-    text-align: center;
-    color: rgb(27, 38, 44);
-
-    margin-bottom: 8px;
-    width: max-content;
-
-    &:after{
-        content: "";
-        height: 3px;
-        width: 80px;
-        background-color: #C4DFAA;
-        top: 14px;
-        display: block;
-    }
-`
-export const ModalButtonClose = styled.button`
-    background-color: transparent;
-    font-size: 1.4em;
-
-    cursor: pointer;
-
-    &:hover{
-        color: var(--colorPlayers)
     }
 `
