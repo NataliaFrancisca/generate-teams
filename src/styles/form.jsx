3ea2c22a0788d6394a6ef31d9bd1865cb4036955
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-18 14:17:36 -0300

    style: remove the elements that was not using, and using button with the type

--- src/styles/form.jsx
+++ src/styles/form.jsx
@@ -2,28 +2,28 @@ import styled from "styled-components";
 
 export const Form = styled.form`
     margin: 10px 0;
-`
-export const FormButton = styled.button`
-    width: 100%;
-    border-radius: 4px;
 
-    margin-top: 15px;
-    padding: 15px 10px;
+    & button[type=submit]{
+        width: 100%;
+        border-radius: 4px;
 
-    font-size: 1em;
-    font-weight: 700;
-    font-family: var(--title);
-    letter-spacing: 2px;
+        margin-top: 15px;
+        padding: 15px 10px;
 
-    background-color: #C4DFAA;
-    cursor: pointer;
-    transition: filter .6s ease-in-out;
+        font-size: 1em;
+        font-weight: 700;
+        font-family: var(--title);
+        letter-spacing: 2px;
 
-    &:hover{
-        filter: brightness(80%);
+        background-color: #C4DFAA;
+        cursor: pointer;
+        transition: filter .6s ease-in-out;
+
+        &:hover{
+            filter: brightness(80%);
+        }
     }
 `
-
 export const GroupLabelInput = styled.div`
     display: flex;
     flex-direction: column;
@@ -49,74 +49,3 @@ export const GroupLabelInput = styled.div`
     }
 `
 
-export const FormTeams = styled.form`
-    display: flex;
-    justify-content: space-around;
-    align-self: flex-start;
-    align-items: center;
-    padding: 10px;
-    border-radius: 5px;
-    border: solid black 2px;
-    margin: 10px 0;
-
-    & label{
-        font-weight: 600;
-        font-size: 1.2em
-    }
-
-    @media screen and (max-width: 500px){
-        flex-direction: column;
-    }
-`
-
-export const InputSpinner = styled.div`
-    width: max-content;
-    padding: 10px;
-    margin-left: 30px;
-
-    background-color: #F9F2ED;
-    box-shadow: 0px 0px 6px #c5bfba;
-
-    border-radius: 10px;
-
-    display: flex;
-    align-items: center;
-    justify-content: space-around;
-
-    font-family: var(--title);
-
-    @media screen and (max-width: 500px){
-        width: 100%;
-        margin-left: 0px;
-        margin: 10px 0;
-    }
-
-    & span, button{
-        font-size: 1.5em;
-        padding: 5px 18px;
-        font-weight: 600;
-    }
-
-    & button{
-        border-radius: 5px;
-        background-color: #C4DFAA;
-        cursor: pointer;
-        transition: filter .4s ease-in-out;
-
-        &:hover{
-            filter: brightness(80%);
-        }
-
-        @media screen and (max-width: 500px){
-            width: 100%;
-        }   
-    }
-
-    & button:first-child{
-        margin-right: 5px;
-    }
-
-    & button:last-child{
-        margin-left: 5px;
-    }
-` 
