42ceaa57b8805df5880f4216ffd0b7bf51e9f834
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-18 16:08:05 -0300

    style: add FloatButotn

--- src/pages/PagePlayers/style.jsx
+++ src/pages/PagePlayers/style.jsx
@@ -9,4 +9,23 @@ export const PlayersStyled = styled.section`
 
     position: relative;
 `
+export const FloatButton = styled.button`
+    position: fixed;
+    bottom: 10px;
+    right: 10px;
 
+    width: 50px;
+    height: 50px;
+    border-radius: 50%;
+
+    box-shadow: 0px 0px 2px rgba(20,20,20,.5);
+    background-color: #C4DFAA;
+
+    transition: filter .6s ease-in-out;
+
+    cursor: pointer;
+
+    &:hover{
+        filter: brightness(80%);
+    }
+`
