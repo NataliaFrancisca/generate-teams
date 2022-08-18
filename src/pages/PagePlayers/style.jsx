61c3ce35091a4574e6ea60bca175511c258aa0a0
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-18 14:15:40 -0300

    style: create component styled

---
+++ src/pages/PagePlayers/style.jsx
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+
+export const PlayersStyled = styled.section`
+    display: flex;
+    flex-direction: column;
+    min-height: 100vh;
+    width: 100%;
+    padding: 10px;
+
+    position: relative;
+`
+
