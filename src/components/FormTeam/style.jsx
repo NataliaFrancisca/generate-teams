d0e4bcdce7246492137d7cac8ead2db78f670faf
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-18 14:12:30 -0300

    style: create InputSpinner styled

---
+++ src/components/FormTeam/style.jsx
@@ -0,0 +1,50 @@
+import styled from "styled-components"
+
+export const InputSpinner = styled.form`
+    width: max-content;
+    padding: 10px;
+
+    background-color: #F9F2ED;
+    box-shadow: 0px 0px 6px #c5bfba;
+
+    border-radius: 10px;
+
+    display: flex;
+    align-items: center;
+    align-self: flex-end;
+    justify-content: space-around;
+
+    font-family: var(--title);
+
+    position: absolute;
+    top: 10px;
+
+    & span, button{
+        font-size: 1.5em;
+        padding: 5px 18px;
+        font-weight: 600;
+    }
+
+    & button{
+        border-radius: 5px;
+        background-color: #C4DFAA;
+        cursor: pointer;
+        transition: filter .4s ease-in-out;
+
+        &:hover{
+            filter: brightness(80%);
+        }
+
+        @media screen and (max-width: 500px){
+            width: 100%;
+        }   
+    }
+
+    & button:first-child{
+        margin-right: 5px;
+    }
+
+    & button:last-child{
+        margin-left: 5px;
+    }
+` 
