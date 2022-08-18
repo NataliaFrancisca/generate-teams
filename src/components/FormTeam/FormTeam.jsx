8830facac015c692dde6e0d1f74f7d8bd5609a2e
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-18 15:58:15 -0300

    feat: change function that handle spinner buttons

--- src/components/FormTeam/FormTeam.jsx
+++ src/components/FormTeam/FormTeam.jsx
@@ -1,58 +1,47 @@
-import { useEffect, useState, useRef } from "react";
+import { useState, useRef } from "react";
 
 import { InputSpinner } from "./style";
 
 import { getPlayers } from "../../functions/registerPlayer"; 
-import { setTeams, setPlayersLeftOver } from "../../functions/registerTeams";
-import { generateTeams, getPlayersLeftOver, generateRandomListOfPlayers } from "../../functions/createTeams";
+import { setTeams } from "../../functions/registerTeams";
+
+import { generateTeams, generateRandomListOfPlayers } from "../../functions/createTeams";
 
 const FormTeam = ({onUpdateStates}) => {
 
     const [numberOfPlayers, setNumberOfPlayers] = useState(2);
-    const [randomListPlayers, setRandomListPlayers] = useState();
-
     const refButtonDecrement = useRef();
     const refButtonIncrement = useRef();
 
-    const onHandleForm = () => {
-        event.preventDefault();
-        if(numberOfPlayers < 2 || numberOfPlayers > getPlayers().length){
-            console.log("Hey, this number isn't possible! Choose another")
+    const totalPlayers = getPlayers().length;
+
+    const onHandleForm = (e) => {
+        e.preventDefault();
+
+        if(numberOfPlayers < 2 || numberOfPlayers > totalPlayers){
             return false;
         } 
         createTeams();
-        handleFormButtons();
+        handleSpinnerButtons();
     }
 
     const createTeams = () => {
         const randomListPlayers = generateRandomListOfPlayers();
-
         const teams = generateTeams(numberOfPlayers, randomListPlayers);
-        const playersLeftOver = getPlayersLeftOver(randomListPlayers, teams);
-
         setTeams(teams);
-        setPlayersLeftOver(playersLeftOver);
-        setRandomListPlayers(randomListPlayers)
         onUpdateStates();
     }
 
     const increment = () => setNumberOfPlayers(numberOfPlayers + 1);
     const decrement = () => setNumberOfPlayers(numberOfPlayers - 1);
 
-    const handleFormButtons = () => {
-        if(numberOfPlayers <= 2){
-            refButtonDecrement.current.disabled = true;
-        }else{
-            refButtonDecrement.current.disabled = false;
-        }
+    const handleSpinnerButtons = () => {
+        const handleDecrementButton = numberOfPlayers <= 2 ? true : false;
+        const handleIncrementButton = numberOfPlayers >= (totalPlayers / 2) ? true : false;
+        refButtonDecrement.current.disabled = handleDecrementButton;
+        refButtonIncrement.current.disabled = handleIncrementButton;
     }
     
-    useEffect(() => {
-       if(randomListPlayers){
-        numberOfPlayers >= randomListPlayers.length  ? refButtonIncrement.current.disabled = true : refButtonIncrement.current.disabled = false;
-       }
-    },[numberOfPlayers])
-
     return(
         <InputSpinner onSubmit={onHandleForm}>
             <button onClick={decrement} ref={refButtonDecrement}>-</button>
