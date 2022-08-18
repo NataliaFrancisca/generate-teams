02b299ac916b138812c7d9dbeb5ad0798cc4ad81
NataliaFrancisca <nathifrancisca@gmail.com>
2022-08-18 16:08:28 -0300

    delete file

--- src/utils/formatString.jsx
+++
@@ -1,7 +0,0 @@
-export const firstLetterUpperCase = (text) => {
-    return text.charAt(0).toUpperCase() + text.slice(1);
-}
-
-export const formatString = (text) => {
-    return text.split(" ").map(string => firstLetterUpperCase(string)).join(" ");
-}
