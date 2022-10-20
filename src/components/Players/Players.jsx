5e35e0c23e1797b9cd21be9f63b450dc91aa9d0f
Natalia Francisca <nathifrancisca@gmail.com>
2022-10-20 10:11:07 -0300

    feat: change the component

--- src/components/Players/Players.jsx
+++ src/components/Players/Players.jsx
@@ -1,28 +1,50 @@
 import React, { useEffect, useState } from "react";
-import { useSelector } from "react-redux";
+import { useSelector, useDispatch } from "react-redux";
 
 import CardPlayer from "../CardPlayer/CardPlayer";
-import ListPlayers from "../ListPlayers/ListPlayers";
+import { ContainerPlayers_styles, ListOfPLayers_styles } from "./PlayersStyle";
 
-import { ContainerListPlayers } from "./style";
+const Players = ({currentListOfPlayers, onUpdateState}) => {
 
-const Players = ({listOfPlayers, onUpdateState}) => {
+    const dispatch = useDispatch();
 
-    const [statePlayers, setStatePlayers] = useState();
+    const [listOfPlayers, setListOfPlayers] = useState();
+
     const playerSelected = useSelector(state => state.reducerSavePlayerData);
+    const setPlayer = data => dispatch({type: "set/player/form", payload: data});
 
     useEffect(() => {
-        setStatePlayers(listOfPlayers);
-    },[listOfPlayers])
+        setListOfPlayers(currentListOfPlayers);
+    },[currentListOfPlayers])
 
-    const isNotEmpty = (arr) => Array.isArray(arr) && arr.length == 0 ? false : true;
+    const arrIsNotEmpty = (arr) => {
+        return Array.isArray(arr) && arr.length > 0 ? true : false;
+    }
 
+    const objIsNotEmpty = (obj) => {
+        return Object.keys(obj).length !== 0 && obj.constructor === Object;
+    } 
+   
     return(
-        <ContainerListPlayers>
-            <ListPlayers players={statePlayers} />
-            {isNotEmpty(playerSelected) && <CardPlayer playerSelected={playerSelected} onUpdateStates={onUpdateState}/>}
-        </ContainerListPlayers>
+        <ContainerPlayers_styles>
+
+            {arrIsNotEmpty(listOfPlayers) &&
+                <ListOfPLayers_styles>
+                    {listOfPlayers.map(player => (
+                        <div key={player.id} className="player-mini-card" onClick={() => setPlayer(player)}>
+                            {player.name}
+                        </div>
+                    ))}
+                </ListOfPLayers_styles>
+            }   
+
+            {objIsNotEmpty(playerSelected) &&
+                <CardPlayer playerSelected={playerSelected} onUpdateStates={onUpdateState}/>
+            }
+            
+        </ContainerPlayers_styles>
     )
+
 }
 
 export default Players;
